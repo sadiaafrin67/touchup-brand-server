86fe866b1eda31ef813abce9a00fedbc6d9cd738
sadiaafrin67 <sa540276@gmail.com>
2023-10-20 00:38:35 +0600

    add my updated coffee server site

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 5000
-
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mnum3sy.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri)
-
-
-
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -33,30 +27,65 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const productsCollection = client.db("touchupDB").collection("products");
 
-    const productsCollection = client.db('touchupDB').collection('products');
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.get('/products', async (req, res) => {
-        const cursor = productsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
-    })
+    app.get("/products/category/:category", async (req, res) => {
+      const category = req.params.category;
+      const query = { category: category };
+      const cursor = productsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
 
-    app.post ('/products', async (req, res) => {
-        const newProduct = req.body;
-        console.log(newProduct)
-        const result = await productsCollection.insertOne(newProduct);
-        res.send(result)
-    })
-
-
-
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+      const product = {
+        $set: {
+          name: updatedProduct.name,
+          brand: updatedProduct.brand,
+          category: updatedProduct.category,
+          image: updatedProduct.image,
+          price: updatedProduct.price,
+          rating: updatedProduct.rating,
+          description: updatedProduct.description,
+        },
+      };
+      const result = await productsCollection.updateOne(
+        filter,
+        product,
+        options
+      );
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -64,28 +93,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Beauty brand server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Beauty brand server is running");
+});
 
 app.listen(port, () => {
-    console.log(`Beauty brand server is running on port ${port}`)
-})
+  console.log(`Beauty brand server is running on port ${port}`);
+});
